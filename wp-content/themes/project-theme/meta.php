8720cf5dbd277f3e04629d0e89e13ff45700a2d7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-04-04 08:05:32 +0900

    Create meta.php

---
+++ wp-content/themes/project-theme/meta.php
@@ -0,0 +1,12 @@
+<?php $root_url = get_template_directory_uri(); ?>
+
+<meta name="viewport" content="width=1200, user-scalable=yes">
+<title><?php wp_title(' | ', true, 'right'); ?>PROJECT THEME</title>
+<link rel="stylesheet" href="<?php echo $root_url; ?>/style.css">
+<script src="<?php echo $root_url; ?>/bundle.js"></script>
+<!-- Google Analytics -->
+<!-- End Google Analytics -->
+
+<!-- Facebook https://developers.facebook.com/docs/plugins/share-button -->
+<!-- Twitter https://publish.twitter.com/# -->
+<!-- LINE https://social-plugins.line.me/ja/ -->
