0b434a48b3062348deffb48bbfd7894fea52b04c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-04-06 06:52:25 +0900

    Create top page

---
+++ wp-content/themes/project-theme/pages/top-page.php
@@ -0,0 +1,9 @@
+<section class="page top-page">
+  <?php get_template_part('components/header') ?>
+
+  <section class="top-page--content">
+    <h2>MAIN CONTENT</h2>
+  </section>
+
+  <?php get_template_part('components/footer') ?>
+</section>
