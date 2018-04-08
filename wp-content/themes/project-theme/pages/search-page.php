c0a17606615a3d97e83dd400d664c9b76c197e4c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-04-09 06:09:15 +0900

    Fix markup for search page

--- wp-content/themes/project-theme/pages/search-page.php
+++ wp-content/themes/project-theme/pages/search-page.php
@@ -7,23 +7,22 @@
 <section class="page search-page">
   <?php get_template_part('components/header') ?>
 
-  <section class="top-page--content">
+  <section class="search-page--content">
     <?php echo $search_query; ?>: <span><?php echo $total_results; ?> results</span>
 
-    <div class="search-page--content">
-      <div class="search-page--content--main">
-        <?php while ( have_posts() ) : the_post(); ?>
-          <?php $post_id = get_the_ID(); ?>
-            <a href="<?php the_permalink() ?>">
-              <?php the_title(); ?>
-              <?php the_excerpt(); ?>
-            </a>
-          <?php endwhile; ?>
-          <?php if ($total_result <= 0): ?>
-            Your search - <?php echo $search_query; ?> - did not match any documents.
-          <?php endif; ?>
-      </div>
+    <div class="search-page--content--main">
+      <?php while ( have_posts() ) : the_post(); ?>
+        <?php $post_id = get_the_ID(); ?>
+          <a href="<?php the_permalink() ?>">
+            <?php the_title(); ?>
+            <?php the_excerpt(); ?>
+          </a>
+        <?php endwhile; ?>
+        <?php if ($total_result <= 0): ?>
+          Your search - <?php echo $search_query; ?> - did not match any documents.
+        <?php endif; ?>
     </div>
+  </section>
 
   <?php get_template_part('components/footer') ?>
 </section>
